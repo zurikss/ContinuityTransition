0fcd8fa9be64216e131395a3a61f850af1a7d091
Zurikss <victorander18@gmail.com>
2024-03-23 19:43:09 +0100

    Initial Commit

---
+++ ContinuityTransition/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ContinuityTransition
+//
+//  Created by Victor on 23/03/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ContinuityTransition/ContinuityTransitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  ContinuityTransitionApp.swift
+//  ContinuityTransition
+//
+//  Created by Victor on 23/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ContinuityTransitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
